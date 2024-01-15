41e35452d5e13e56df1c2e18b7bffe210523e3b7
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2024-01-15 12:20:22 +0530

    Add force dynamic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import Container from "@/components/Container";
-import ListBox from "@/components/listings/ListBox";
+import getListings from "@/actions/getListings";
+import Empty from "@/components/Empty";
+import getCurrentUser from "@/actions/getCurrentUser";
+import ListingCard from "@/components/listings/ListingCard";
+
+export const dynamic = 'force-dynamic';
 
 interface HomeProps {
   searchParams: {
@@ -15,11 +20,22 @@ interface HomeProps {
   };
 }
 
-const Home = ({ searchParams }: HomeProps) => {
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+  if (!listings) return <Empty showReset />;
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        <ListBox params={searchParams} />
+        {listings.map((listing) => {
+          return (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          );
+        })}
       </div>
     </Container>
   );
